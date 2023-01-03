4c9af46a8c4133105ca5ba3c6e85c79f6f7fd65e
PATTARAPOL NA NAKORN <pattarapol_nanakorn@cmu.ac.th>
2023-01-03 15:51:06 +0700

    Complete

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,30 @@
 //cout << "Number of data = ";
 //cout << "Mean = ";
 //cout << "Standard deviation = ";
+
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<cstdlib>
+#include<iomanip>
+#include<cmath>
+using namespace std;
+
+int main (){
+    string textline;
+	ifstream score("score.txt");
+    float n = 0;
+    float sum = 0,mean =0;
+    float total = 0;
+    while(getline(score,textline)){
+        n++;
+        sum += atof(textline.c_str());
+        total += pow(atof(textline.c_str()),2);
+    }
+    mean = sum/n;
+    cout << total << endl;
+    cout << "Number of data = " << n << endl;
+    cout << setprecision(3);
+    cout << "Mean = " << mean << endl;
+    cout << "Standard deviation = " << sqrt(((1/n)*total)-pow(mean,2));
+    }
